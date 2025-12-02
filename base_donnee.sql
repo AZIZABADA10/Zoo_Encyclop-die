f10d5f95c3a9f853debfc33f2673db2a7b718cc7
AZIZABADA10 <abadaaziz288@gmail.com>
2025-12-02 15:44:14 +0000

    add:    l'ajoute de scripts pour créer et manipuler la base de donnée (ref #3)

--- base_donnee.sql
+++ base_donnee.sql
@@ -35,3 +35,22 @@ INSERT INTO animal (nom, type_alimentaire, image_animal, id_habitat) VALUES
 ('Tigre', 'Carnivore', 'tigre.jpg', 2),
 ('Dauphin', 'Carnivore', 'dauphin.jpg', 3),
 ('Chameau', 'Herbivore', 'chameau.jpg', 4);
+
+
+/*Modification*/
+update animal set nom = 'lion1'where id = 1 ;
+update habitats set nom_habitat = 'Jungle1'where id = 1 ;
+
+/*Suppression d'un animal : */
+delete from animal where id = 1;
+delete from animal where type_alimentaire = 'Carnivore';
+
+
+/*Affichage des animaux du zoo*/
+/*Affichage de tout les animaux*/
+select * from animal;
+
+/*Filtrage par type alimentaire */
+select * from animal where type_alimentaire = 'Carnivore';
+/*Filtrage par Habitat*/
+select nom,nom_habitat from animal,habitats where animal.id_habitat = habitats.id and nom_habitat='Désert';
