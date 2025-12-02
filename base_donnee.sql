207b13797dd0af5f169e07481814380ae7ae7a3d
AZIZABADA10 <abadaaziz288@gmail.com>
2025-12-02 14:36:26 +0000

    add:    l'ajoute de scripts pour créer et manipuler la base de données.

---
+++ base_donnee.sql
@@ -0,0 +1,18 @@
+/*creation du base de donnée*/
+CREATE database zoo;
+use zoo;
+/*la creation des tables sur la base de donnée*/
+CREATE TABLE habitats (
+    id int primary key AUTO_INCREMENT,
+    nom_habitat varchar(250),
+    description_habitat varchar(250)
+)
+
+CREATE TABLE animal (
+    id int primary key AUTO_INCREMENT,
+    nom varchar(150),
+    type_alimentaire varchar(100),
+    image_animal varchar(250),
+    id_habitat int,
+    FOREIGN KEY (id_habitat) REFERENCES habitats(id)
+);
